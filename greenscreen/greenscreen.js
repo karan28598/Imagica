c856e61034b8132c6960bc70f4ba001250031485
karan28598 <mr.karanagrawal@gmail.com>
2017-05-02 02:57:17 -0530

    add greenscreen.js

---
+++ greenscreen/greenscreen.js
@@ -0,0 +1,93 @@
+//Grayscale
+var image = null;
+
+function upload() {
+  //Get input from file input
+  var fileinput = document.getElementById("finput");
+  //Make new SimpleImage from file input
+  image = new SimpleImage(fileinput);
+  //Get canvas
+  var canvas = document.getElementById("can");
+  //Draw image on canvas
+  image.drawTo(canvas);
+}
+
+function makeGray() {
+  //change all pixels of image to gray
+  for (var pixel of image.values()) {
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    pixel.setRed(avg);
+    pixel.setGreen(avg);
+    pixel.setBlue(avg);
+  }
+  //display new image
+  var canvas = document.getElementById("can");
+  image.drawTo(canvas);
+}
+
+
+
+//Green Screen
+var fgImage = null;
+var bgImage = null;
+var fgCanvas;
+var bgCanvas;
+
+function loadForegroundImage() {
+  var file = document.getElementById("fgfile");
+  fgImage = new SimpleImage(file);
+  fgCanvas = document.getElementById("fgcan");
+  fgImage.drawTo(fgCanvas);
+}
+
+function loadBackgroundImage() {
+  var file = document.getElementById("bgfile");
+  bgImage = new SimpleImage(file);
+  bgCanvas = document.getElementById("bgcan");
+  bgImage.drawTo(bgCanvas);
+}
+
+function createComposite() {
+  // this function creates a new image with the dimensions of the foreground image and returns the composite green screen image
+  var output = new SimpleImage(fgImage.getWidth(), fgImage.getHeight());
+  var greenThreshold = 240;
+  for (var pixel of fgImage.values()) {
+    var x = pixel.getX();
+    var y = pixel.getY();
+    if (pixel.getGreen() > greenThreshold) {
+      //pixel is green, use background
+      var bgPixel = bgImage.getPixel(x, y);
+      output.setPixel(x, y, bgPixel);
+    }
+    else {
+      //pixel is not green, use foreground
+      output.setPixel(x, y, pixel);
+    }
+  }
+  return output;
+}
+
+function doGreenScreen() {
+  //check that images are loaded
+  if (fgImage == null || !fgImage.complete()) {
+    alert("Foreground image not loaded");
+  }
+  if (bgImage == null || !bgImage.complete()) {
+    alert("Background image not loaded");
+  }
+  // clear canvases
+  clearCanvas();
+  // call createComposite, which does green screen algorithm and returns a composite image
+  var finalImage = createComposite();
+  finalImage.drawTo(fgCanvas);
+}
+
+function clearCanvas() {
+  doClear(fgCanvas);
+  doClear(bgCanvas);
+}
+
+function doClear(canvas) {
+  var context = canvas.getContext("2d");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
