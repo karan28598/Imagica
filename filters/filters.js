385a7143643ffb5b47854346eeab279efb85077f
karan28598 <mr.karanagrawal@gmail.com>
2017-05-04 04:05:20 -0530

    add rainbow filter

--- filters/filters.js
+++ filters/filters.js
@@ -1,3 +1,4 @@
+//Grayscale
 var image = null;
 
 function upload() {
@@ -22,4 +23,106 @@ function makeGray() {
   //display new image
   var canvas = document.getElementById("can");
   image.drawTo(canvas);
+}
+
+
+//Rainbow
+function filterRainbow() {
+  var height = image.getHeight();
+  for (var pixel of image.values()) {
+    var y = pixel.getY();
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    if (y < height / 7) {
+      //red
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (y < height * 2 / 7) {
+      //orange
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(0.8 * avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(1.2 * avg - 51);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (y < height * 3 / 7) {
+      //yellow
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(2 * avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (y < height * 4 / 7) {
+      //green
+      if (avg < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(2 * avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(2 * avg - 255);
+        pixel.setGreen(255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (y < height * 5 / 7) {
+      //blue
+      if (avg < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(2 * avg);
+      } else {
+        pixel.setRed(2 * avg - 255);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(255);
+      }
+    } else if (y < height * 6 / 7) {
+      //indigo
+      if (avg < 128) {
+        pixel.setRed(.8 * avg);
+        pixel.setGreen(0);
+        pixel.setBlue(2 * avg);
+      } else {
+        pixel.setRed(1.2 * avg - 51);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(255);
+      }
+    } else {
+      //violet
+      if (avg < 128) {
+        pixel.setRed(1.6 * avg);
+        pixel.setGreen(0);
+        pixel.setBlue(1.6 * avg);
+      } else {
+        pixel.setRed(0.4 * avg + 153);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(0.4 * avg + 153);
+      }
+    }
+  }
+  //display new image
+  var canvas = document.getElementById("can");
+  image.drawTo(canvas);
+}
+
+
+//Clear Canvas
+function clearCanvas() {
+  doClear(can);
+}
+
+function doClear(canvas) {
+  var context = canvas.getContext("2d");
+  context.clearRect(0, 0, canvas.width, canvas.height);
 }
